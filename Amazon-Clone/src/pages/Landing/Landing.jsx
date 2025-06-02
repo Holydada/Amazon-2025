037137fd2e93a4a145320f0ecebfe6dd95c94541
Holydada <jesuschristmydestiny@gmail.com>
2025-06-03 01:31:17 +0300

    commit6

--- Amazon-Clone/src/pages/Landing/Landing.jsx
+++ Amazon-Clone/src/pages/Landing/Landing.jsx
@@ -3,7 +3,7 @@ import Layout from '../../Components/Layout/Layout';
 import CarouselEffect from '../../Components/Carousel/CarouselEffect'
 import Product from '../../Components/Product/Product'
 
-import CatagoryEffect from '../../components/Catagory/CatagoryEffect'
+import CatagoryEffect from '../../Components/Catagory/CatagoryEffect'
 
 
 function Landing() {
