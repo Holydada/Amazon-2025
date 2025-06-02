342a1dfed7b1b446bee096d64e415e3320d329a8
Holydada <jesuschristmydestiny@gmail.com>
2025-06-03 01:27:47 +0300

    commi4

--- Amazon-Clone/src/pages/Landing/Landing.jsx
+++ Amazon-Clone/src/pages/Landing/Landing.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Layout from '../../components/Layout/Layout';
+import Layout from '../../Components/Layout/Layout';
 import CarouselEffect from '../../components/Carousel/CarouselEffect'
 import Product from '../../components/Product/Product'
 
